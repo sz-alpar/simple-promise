2f6a19c439317cfb8d828e57805aaeeac08423b6
Alpar Szotyori <a.szotyori@gini.net>
2016-08-02 17:59:36 +0200

    Added classes from the Android project

---
+++ test/java/com/repeatuntil/simplepromise/CustomObject.java
@@ -0,0 +1,11 @@
+package com.repeatuntil.simplepromise;
+
+import java.util.List;
+
+public class CustomObject {
+    public final List<Integer> someList;
+
+    public CustomObject(List<Integer> someList) {
+        this.someList = someList;
+    }
+}
